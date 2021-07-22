749bcfcaf00faac6255f3eefd783d9c8c297761b
Mykola Myslovskyi <adriandecita@gmail.com>
2021-07-23 00:28:15 +0300

    initial commit. Cloned template with basic customizations

---
+++ src/pages/Index.vue
@@ -0,0 +1,114 @@
+<template>
+  <Layout>
+    <div class="pt-8 md:pt-16">
+      <div class="flex flex-col items-center">
+        <div class="flex flex-col items-center mb-2 text-ui-primary">
+          <Logo :width="80" />
+          <h2 class="text-3xl text-6xl font-black tracking-tighter border-none">
+            WebDoky
+          </h2>
+        </div>
+        <h1 class="text-4xl text-center lg:text-5xl">
+          Ресурси для розробників, від розробників
+        </h1>
+        <div class="flex justify-space-between">
+          <a href="/docs/Web">Веб-технології &#8594;</a>
+        </div>
+        
+
+      </div>
+
+      <div class="pt-8 mx-auto mt-8 border-t md:mt-16 md:pt-16 border-top border-ui-border max-w-screen-sm"></div>
+
+      <div class="flex flex-wrap justify-center -mx-4">
+        <div class="flex flex-col items-center w-full px-4 mb-8 text-center md:w-1/3">
+          <ZapIcon size="3x" class="mb-6 text-ui-primary" />
+          <h3 class="font-bold tracking-wide uppercase text-ui-primary">
+            Incredibly Fast
+          </h3>
+          <p class="text-lg text-left">
+            Powered by Gridsome. Built on Vue. Outputs static files. It wont't get any faster than this. <span class="border-b border-dashed border-ui-primary text-ui-primary">No seriously</span>.
+          </p>
+        </div>
+
+        <div class="flex flex-col items-center w-full px-4 mb-8 text-center md:w-1/3">
+          <SearchIcon size="3x" class="mb-6 text-ui-primary" />
+          <h3 class="font-bold tracking-wide uppercase text-ui-primary">
+            Instant Search
+          </h3>
+          <p class="text-lg text-left">
+            All headlines are indexed. Instant search powered by Fuse.js. Find what you are looking for in the <span class="border-b border-dashed border-ui-primary text-ui-primary">blink of an eye</span>.
+          </p>
+        </div>
+
+        <div class="flex flex-col items-center w-full px-4 mb-8 text-center md:w-1/3">
+          <MoonIcon size="3x" class="mb-6 text-ui-primary" />
+          <h3 class="font-bold tracking-wide uppercase text-ui-primary">
+            Lights out
+          </h3>
+          <p class="text-lg text-left">
+            Working late at night again? Enable dark mode with the click of a button.
+          </p>
+        </div>
+      </div>
+
+    </div>
+  </Layout>
+</template>
+
+<script>
+import Logo from '@/components/Logo';
+import { ArrowRightCircleIcon, ZapIcon, CodeIcon, MoonIcon, SearchIcon } from 'vue-feather-icons';
+
+export default {
+  components: {
+    Logo,
+    ArrowRightCircleIcon,
+    ZapIcon,
+    CodeIcon,
+    MoonIcon,
+    SearchIcon
+  },
+
+  metaInfo() {
+    const title = 'Great Documentation starts here';
+    const description = 'DOCC is a starter theme with instant search and dark mode for writing great technical documentation. Based on Gridsome!';
+
+    return {
+      title: title,
+      meta: [
+        {
+          name: 'description',
+          content: description
+        },
+        {
+          key: 'og:title',
+          name: 'og:title',
+          content: title,
+        },
+        {
+          key: 'twitter:title',
+          name: 'twitter:title',
+          content: title,
+        },
+        {
+          key: 'og:description',
+          name: 'og:description',
+          content: description,
+        },
+        {
+          key: 'twitter:description',
+          name: 'twitter:description',
+          content: description,
+        },
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+.home-links a {
+  margin-right: 1rem;
+}
+</style>
