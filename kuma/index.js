6219df357a039be75f7ef460f690d316ee18d847
Mykola Myslovskyi <adriandecita@gmail.com>
2021-08-02 23:09:35 +0300

    add JS main navigation

--- kuma/index.js
+++ kuma/index.js
@@ -1,6 +1,9 @@
 const { default: CSSRef, navLabels: CSSRefLabels} = require('./macros/css-ref');
+const { default: JsSidebar, navLabels: JSRefLabels} = require('./macros/js-sidebar');
 
 module.exports = {
     CSSRef,
     CSSRefLabels,
+    JsSidebar,
+    JSRefLabels,
 }
--- src/components/WebdocNav.vue
+++ src/components/WebdocNav.vue
@@ -5,15 +5,18 @@
     class="px-4 pt-8 lg:pt-12"
   >
     <nav-css v-if="showSidebar === 'CSSRef'" :allPages="allPages" :currentPage="currentPage" />
+    <nav-js v-if="showSidebar === 'JsSidebar'" :allPages="allPages" :currentPage="currentPage" />
   </div>
 </template>
 
 <script>
 import NavCss from '@/components/NavCss.vue';
+import NavJs from '@/components/NavJs.vue';
 
 export default {
   components: {
     NavCss,
+    NavJs
   },
   props: ['showSidebar', 'allPages', 'currentPage'], 
 };
