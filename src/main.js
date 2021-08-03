0585ba8321c072d892e4edb6d6390957108a326e
Mykola Myslovskyi <adriandecita@gmail.com>
2021-08-03 23:08:45 +0300

    Add typescript and eslint with prettier
    * add typescript and eslint with prettier

--- src/main.js
+++ src/main.js
@@ -1,19 +1,19 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
-import DefaultLayout from '~/layouts/Default.vue'
-import WebdocPageLayout from '~/layouts/WebdocPage.vue'
+import DefaultLayout from '~/layouts/Default.vue';
+import WebdocPageLayout from '~/layouts/WebdocPage.vue';
 
 export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
-  Vue.component('Layout', DefaultLayout)
-  Vue.component('WebdocPageLayout', WebdocPageLayout)
+  Vue.component('Layout', DefaultLayout);
+  Vue.component('WebdocPageLayout', WebdocPageLayout);
 
   router.beforeEach((to, _from, next) => {
     head.meta.push({
       key: 'og:url',
       name: 'og:url',
       content: process.env.GRIDSOME_BASE_PATH + to.path,
-    })
-    next()
-  })
+    });
+    next();
+  });
 }
