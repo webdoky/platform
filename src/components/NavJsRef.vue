5ad9d94acd912f479ad1949c6cc17c95e4a3ce50
Mykola Myslovskyi <adriandecita@gmail.com>
2021-08-05 23:10:01 +0300

    fix some bugs and exceptions

--- src/components/NavJsRef.vue
+++ src/components/NavJsRef.vue
@@ -37,10 +37,20 @@
       </nav-sub-menu>
     </div>
 
-    <h3 class="pt-0 mt-0 mb-1 text-sm tracking-tight border-none">
+    <h3
+      v-if="
+        functionInheritanceSections.length && objectInheritanceSections.length
+      "
+      class="pt-0 mt-0 mb-1 text-sm tracking-tight border-none"
+    >
       Inheritance:
     </h3>
-    <h3 class="pt-0 mt-0 mb-1 text-sm tracking-tight border-none">Function:</h3>
+    <h3
+      v-if="functionInheritanceSections.length"
+      class="pt-0 mt-0 mb-1 text-sm tracking-tight border-none"
+    >
+      Function:
+    </h3>
     <div
       v-for="(section, index) in functionInheritanceSections"
       :key="'function' + section.title"
@@ -66,7 +76,12 @@
         </ul>
       </nav-sub-menu>
     </div>
-    <h3 class="pt-0 mt-0 mb-1 text-sm tracking-tight border-none">Object:</h3>
+    <h3
+      v-if="objectInheritanceSections.length"
+      class="pt-0 mt-0 mb-1 text-sm tracking-tight border-none"
+    >
+      Object:
+    </h3>
     <div
       v-for="(section, index) in objectInheritanceSections"
       :key="'object' + section.title"
@@ -98,9 +113,7 @@
 <script>
 import NavSubMenu from '@/components/NavSubMenu';
 import NavItem from '@/components/NavItem';
-import { JSRefLabels, inheritanceData, groupData } from '../../kuma';
-
-const locale = `/en-US`;
+import { inheritanceData, groupData } from '../../kuma';
 
 export default {
   components: {
@@ -178,7 +191,10 @@ export default {
           0
       );
       source[objectName] = globalObjectsPages.filter(
-        ({ path }) => path.indexOf(`/en-US/docs/${slug}/`) >= 0
+        ({ path }) =>
+          path.indexOf(
+            `/en-US/docs/Web/JavaScript/Reference/Global_Objects/${objectName}/`
+          ) >= 0
       );
       result[objectName] = {
         title: objectName,
@@ -257,47 +273,55 @@ export default {
     },
     objectInheritanceSections() {
       const { navigationStructure } = this;
-      const { properties: objectProperties, methods: objectMethods } =
-        navigationStructure['iObject'];
+      if (navigationStructure['iObject']) {
+        const { properties: objectProperties, methods: objectMethods } =
+          navigationStructure['iObject'];
 
-      return [
-        {
-          title: 'properties',
-          items: objectProperties,
-          expanded: objectProperties.find(
-            ({ path }) => path === this.currentPage.path
-          ),
-        },
-        {
-          title: 'methods',
-          items: objectMethods,
-          expanded: objectMethods.find(
-            ({ path }) => path === this.currentPage.path
-          ),
-        },
-      ];
+        return [
+          {
+            title: 'properties',
+            items: objectProperties,
+            expanded: objectProperties.find(
+              ({ path }) => path === this.currentPage.path
+            ),
+          },
+          {
+            title: 'methods',
+            items: objectMethods,
+            expanded: objectMethods.find(
+              ({ path }) => path === this.currentPage.path
+            ),
+          },
+        ];
+      }
+
+      return [];
     },
     functionInheritanceSections() {
       const { navigationStructure } = this;
-      const { properties: functionProperties, methods: functionMethods } =
-        navigationStructure['iFunction'];
+      if (navigationStructure['iFunction']) {
+        const { properties: functionProperties, methods: functionMethods } =
+          navigationStructure['iFunction'];
 
-      return [
-        {
-          title: 'properties',
-          items: functionProperties,
-          expanded: functionProperties.find(
-            ({ path }) => path === this.currentPage.path
-          ),
-        },
-        {
-          title: 'methods',
-          items: functionMethods,
-          expanded: functionMethods.find(
-            ({ path }) => path === this.currentPage.path
-          ),
-        },
-      ];
+        return [
+          {
+            title: 'properties',
+            items: functionProperties,
+            expanded: functionProperties.find(
+              ({ path }) => path === this.currentPage.path
+            ),
+          },
+          {
+            title: 'methods',
+            items: functionMethods,
+            expanded: functionMethods.find(
+              ({ path }) => path === this.currentPage.path
+            ),
+          },
+        ];
+      }
+
+      return [];
     },
   },
   methods: {
