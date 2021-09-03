0a973ac3e38a7ec53f6e534afe9211d9e4c8f377
Mykola Myslovskyi <adriandecita@gmail.com>
2021-09-04 00:28:00 +0300

    add licenses, unify code highlighting in internal docs

--- gridsome.config.js
+++ gridsome.config.js
@@ -33,6 +33,7 @@ module.exports = {
               '/docs/requirements/',
               '/docs/review-procedure/',
               '/docs/glossary/',
+              '/docs/licensing/',
             ],
           },
         ],
