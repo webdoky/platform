af137ca17b6ed5b53d2dda457eeb9729685781e2
Mykola Myslovskyi <adriandecita@gmail.com>
2022-02-12 16:48:29 +0200

    chore: add checks against broken macros

--- registry/macros-runner.js
+++ registry/macros-runner.js
@@ -12,6 +12,7 @@ const runMacros = (content, context, navigationOnly = false) => {
   const data = {
     macros: [],
   };
+  const failedMacros = {};
 
   const macrosRegistry = new Macros(context);
 
@@ -38,6 +39,17 @@ const runMacros = (content, context, navigationOnly = false) => {
             }}}. Original error: ${e.message}`
           );
         }
+      } else {
+        failedMacros[functionName];
+        if (failedMacros[functionName]) {
+          failedMacros[functionName].count += 1;
+          failedMacros[functionName].lastUsedExpression = match;
+        } else {
+          failedMacros[functionName] = {
+            count: 0,
+            lastUsedExpression: match,
+          };
+        }
       }
       if (typeof result !== 'string') {
         // if the output is not a string, then we have to additionaly process it in the app
@@ -56,6 +68,15 @@ const runMacros = (content, context, navigationOnly = false) => {
     }
   });
 
+  const numberOfFailedMacros = Object.keys(failedMacros).length;
+  if (numberOfFailedMacros) {
+    console.warn(`Got ${numberOfFailedMacros} failed macros`);
+    Object.entries(failedMacros).forEach(([functionName, entry]) => {
+      console.warn(
+        `${entry.count} failed ${functionName} macros, the last expression was: ${entry.lastUsedExpression}`
+      );
+    });
+  }
   return {
     content: resultContent,
     data,
